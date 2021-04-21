a8a8577e38f415f615d2e3aaf464bcbe659e583a
arcshiftsolutions <arcshiftsolutions@gmail.com>
2021-04-21 16:32:23 -0700

    Fix test.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/api': {
-          target: 'https://sld-api-75e61b-tools.apps.silver.devops.gov.bc.ca',
+          target: 'https://grad-admin-77c02f-tools.apps.silver.devops.gov.bc.ca',
           changeOrigin: true,
           pathRewrite: {
             '^/api': ''
