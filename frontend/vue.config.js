a2850ae99e6f2f73c97eedd62595c9e76a015a93
arcshiftsolutions <arcshiftsolutions@gmail.com>
2021-04-21 17:29:45 -0700

    Testing.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -12,22 +12,7 @@ module.exports = {
     },
     devServer: {
       proxy: {
-        '/api': {
-            target: 'https://gradstudent-api-77c02f-dev.apps.silver.devops.gov.bc.ca',
-            changeOrigin: true
-        },
-        '/api/v1/studentassessment': {
-          target: 'https://student-assessment-api-77c02f-dev.apps.silver.devops.gov.bc.ca',
-          changeOrigin: true
-        },
-        '/api/studentexam': {
-            target: process.env.VUE_APP_STUDENT_EXAMS_API_HOST,
-            changeOrigin: true,
-            pathRewrite: {
-                '^/api/studentexam': ''
-              }
-          },
-  
+
       }
     },
     publicPath: '/'
